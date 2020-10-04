b449a4476752b3828308a2ccbc55c4fe43211c14
Mehmood Ayaz <mahmoodayaz7@gmail.com>
2020-10-05 00:37:38 +0500

    react router / custom layouts / footer components

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.scss';
+
+const Footer = props => {
+    return(
+        <div className="footer">
+            <div className="wrap">
+                Copyright (c) 2020 Hemanical Dreamz
+            </div>
+
+        </div>
+    )
+}
+
+export default Footer;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./styles.scss";
 import logo from "./../../assets/Logo.jpg";
 
@@ -7,7 +8,16 @@ const Header = (props) => {
     <header className="header">
       <div className="wrap">
         <div className="logo">
-          <img src={logo} alt="Hemanical Dreamz Logo" />
+          <Link to="/">
+            <img src={logo} alt="Hemanical Dreamz Logo" />
+          </Link>
+        </div>
+        <div className="callToActions">
+          <ul>
+            <li>
+              <Link to="/registration">Register</Link>
+            </li>
+          </ul>
         </div>
       </div>
     </header>
